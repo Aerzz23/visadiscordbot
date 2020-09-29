e3df50dea19054073a2415f8cd10f57beb2fff63
Aaron Burge <aaronburge23@gmail.com>
2020-09-29 16:06:26 +0100

    - Fixed if statement to return message to user if number of inputs wrong - Added launch.json config for vscode debugging

--- api/handlers/messagecreate.go
+++ api/handlers/messagecreate.go
@@ -39,7 +39,7 @@ func (bh *BotHandlersImpl) MessageCreateHandler(s *discordgo.Session, m *discord
 		log.Println(fmt.Sprintf("NewEvent triggered by %s. Message: %s", m.Author, m.Content))
 		inputSlice := strings.Fields(m.Content)[2:]
 
-		if lengthOfSlice := len(inputSlice); lengthOfSlice < 4 && lengthOfSlice > 6 {
+		if lengthOfSlice := len(inputSlice); lengthOfSlice < 4 || lengthOfSlice > 6 {
 			log.Println(fmt.Sprintf("NewEvent has invalid amount of arguments, was given %s with %d arguments", inputSlice, lengthOfSlice))
 			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Oops <@%s> - it seems like you've given me the wrong amount of arguments. You gave me %d instead of 4 or 6", m.Author.ID, lengthOfSlice))
 			return
